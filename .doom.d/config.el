09d06c54d140511e9f7106da83a98535e71206b8
iszla <karl.hesselgren@gmail.com>
2019-04-21 20:12:13 +0200

    Updated readme, fixed issue in i3 and added doom conf

---
+++ .doom.d/config.el
@@ -0,0 +1,6 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+(setq doom-font (font-spec :family "Fira Code" :size 14))
+(setq  x-meta-keysym 'super
+       x-super-keysym 'meta)
